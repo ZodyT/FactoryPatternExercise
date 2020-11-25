8282dd925898a75ac8d348db21a4fabe3977e16e
ZodyT <Zoraida1988@yahoo.com>
2020-11-25 16:30:41 -0600

    Classes

---
+++ FactoryPatternExercise/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPatternExercise
+{
+    public class Car : IVehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Building a new Car!");
+        }
+    }
+}
